56419f33c6a1308cf13c2f75965f77574d92e7e8
Nicolás Castro <nicolas.castro@bci.cl>
2020-04-11 16:12:29 -0400

    p1-ch6/docker_config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
@@ -5,7 +7,8 @@ app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.environ['APP_SETTINGS']
+app.config.from_object(app_settings)
 
 class UserPing(Resource):
     def get(self):
