5e6dfa3d5da745392ae088218458e95d077dde86
Nicolás Castro <nicolas.castro@bci.cl>
2020-04-11 16:17:32 -0400

    Add flake8 linter

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -10,11 +10,13 @@ api = Api(app)
 app_settings = os.environ['APP_SETTINGS']
 app.config.from_object(app_settings)
 
+
 class UserPing(Resource):
     def get(self):
         return {
             'status': 'success',
             'message': 'pong!',
         }
+
 
 api.add_resource(UserPing, '/users/ping')
