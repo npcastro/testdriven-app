e81da99068396036e84a0d92d10257c3bfe804ec
Nicolás Castro <nicolas.castro@bci.cl>
2020-04-28 19:21:08 -0400

    p2-ch7/react_forms

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,14 +2,22 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import AddUser from './components/AddUser';
 import UsersList from './components/UsersList';
 
 class App extends Component {
   constructor() {
     super();
+    
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     }
+
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
   }
 
   componentDidMount() {
@@ -22,15 +30,45 @@ class App extends Component {
     .catch((err) => { console.log(err); });
   }
 
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({username: '', email: ''});
+      })
+      .catch((err) => { console.log(err); });
+
+    
+  };
+
   render() {
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
-            <div className="column is-one-third">
+            <div className="column is-half">
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <br/><br/>
               <UsersList users={this.state.users}/>
             </div>
           </div>
