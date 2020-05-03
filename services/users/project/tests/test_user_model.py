a5895d749847ac7c721586326bb98a0113a23c1b
Nicolás Castro <nicolas.castro@bci.cl>
2020-05-02 23:12:03 -0400

    p3-ch1/flask_migrate

---
+++ services/users/project/tests/test_user_model.py
@@ -0,0 +1,34 @@
+import unittest
+
+from project.tests.base import BaseTestCase
+from project.tests.utils import add_user
+
+from sqlalchemy.exc import IntegrityError
+
+
+class TestUserModel(BaseTestCase):
+
+    def test_add_user(self):
+        user = add_user('justatest', 'test@test.com')
+        self.assertTrue(user.id)
+        self.assertEqual(user.username, 'justatest')
+        self.assertEqual(user.email, 'test@test.com')
+        self.assertTrue(user.active)
+
+    def test_add_user_duplicate_username(self):
+        add_user('justatest', 'test@test.com')
+        with self.assertRaises(IntegrityError):
+            add_user('justatest', 'test@test2.com')
+
+    def test_add_user_duplicate_email(self):
+        add_user('justatest', 'test@test.com')
+        with self.assertRaises(IntegrityError):
+            add_user('justanothertest', 'test@test.com')
+
+    def test_to_json(self):
+        user = add_user('justatest', 'test@test.com')
+        self.assertTrue(isinstance(user.to_json(), dict))
+
+
+if __name__ == '__main__':
+    unittest.main()
