5a323c6ed3f4ddf4246e85fa3f3a19860c819034
Nicolás Castro Leal <npcastrol@gmail.com>
2020-12-17 23:11:39 -0300

    Add image pulling from ECR

--- docker-pull.sh
+++ docker-pull.sh
@@ -1,8 +1,41 @@
 #!/bin/sh
-echo "Pulling services images from ECR"
 
-# - export REPO=$AWS_ACCOUNT.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME
-# - export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
-# - docker pull $(REPO):$(TRAVIS_BRANCH)
+pull_and_build() {
+  echo "Pulling services images from ECR"
+  export TAG=$TRAVIS_BRANCH
+  env=$1
 
-# - docker build --cache-from $(REPO):$(TRAVIS_BRANCH) -t $(REPO):$(TRAVIS_COMMIT) .
+  ls -l
+  ls -l ./services
+
+  # users
+  docker pull $ECR_REPO/$USERS:$TAG
+  docker build ./services/users --cache-from $ECR_REPO/$USERS:$TAG -f ./services/users/Dockerfile-$env
+
+  # users db
+  docker pull $ECR_REPO/$USERS_DB:$TAG
+  docker build ./services/users/project/db --cache-from $ECR_REPO/$USERS_DB:$TAG -f ./services/users/project/db/Dockerfile
+
+  # client
+  docker pull $ECR_REPO/$CLIENT:$TAG
+  docker build ./services/client --cache-from $ECR_REPO/$CLIENT:$TAG -f ./services/client/Dockerfile-$env
+
+  # swagger
+  docker pull $ECR_REPO/$SWAGGER:$TAG
+  docker build ./services/swagger --cache-from $ECR_REPO/$SWAGGER:$TAG -f ./services/swagger/Dockerfile-$env
+}
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+
+aws ecr get-login-password --region us-east-1 \
+    | docker login --username AWS --password-stdin $ECR_REPO
+
+if [ "$TRAVIS_BRANCH" == "staging" ]
+then
+  pull_and_build stage
+elif [ "$TRAVIS_BRANCH" == "production" ]
+then
+  pull_and_build prod
+fi
