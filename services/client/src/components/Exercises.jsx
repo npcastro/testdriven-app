2690916e634d64172aee333179ff1d87b36a6c8d
Nicolás Castro Leal <npcastrol@gmail.com>
2021-02-04 21:51:07 -0300

    p7-ch3/exercise_component

--- services/client/src/components/Exercises.jsx
+++ services/client/src/components/Exercises.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import AceEditor from 'react-ace';
-import 'brace/mode/python';
-import 'brace/theme/solarized_dark';
+
+import Exercise from './Exercise';
 
 
 class Exercises extends Component {
@@ -76,63 +75,14 @@ class Exercises extends Component {
             <span>Please log in to submit an exercise.</span>
           </div>
         }
-        {this.state.exercises.length &&
-          <div key={this.state.exercises[0].id}>
-            <h5 className="title is-5">{this.state.exercises[0].body}</h5>
-            <AceEditor
-              mode="python"
-              theme="solarized_dark"
-              name={(this.state.exercises[0].id).toString()}
-              onChange={this.onChange}
-              onLoad={this.onLoad}
-              fontSize={14}
-              height={'175px'}
-              showPrintMargin={true}
-              showGutter={true}
-              highlightActiveLine={true}
-              value={this.state.editor.value}
-              style={{
-                marginBottom: '10px'
-              }}
-              editorProps={{
-                $blockScrolling: Infinity
-              }}
-            />
-            {this.props.isAuthenticated &&
-              <div>
-                <button
-                  className="button is-primary"
-                  onClick={this.submitExercise}
-                  disabled={this.state.editor.button.isDisabled}
-                >Run Code</button>
-                {this.state.editor.showGrading &&
-                  <h5 className="title is-5">
-                    <span className="icon is-large">
-                      <i className="fas fa-spinner fa-pulse"></i>
-                    </span>
-                    <span className="grade-text">Grading...</span>
-                  </h5>
-                }
-                {this.state.editor.showCorrect &&
-                  <h5 className="title is-5">
-                    <span className="icon is-large">
-                      <i className="fas fa-check"></i>
-                    </span>
-                    <span className="grade-text">Correct!</span>
-                  </h5>
-                }
-                {this.state.editor.showIncorrect &&
-                  <h5 className="title is-5">
-                    <span className="icon is-large">
-                      <i className="fas fa-times"></i>
-                    </span>
-                    <span className="grade-text">Incorrect!</span>
-                  </h5>
-                }
-              </div>
-            }
-            <br/><br/>
-          </div>
+        {this.state.exercises.length > 0 &&
+          <Exercise
+            exercise={this.state.exercises[0]}
+            editor={this.state.editor}
+            isAuthenticated={this.props.isAuthenticated}
+            onChange={this.onChange}
+            submitExercise={this.submitExercise}
+          />
         }
       </div>
     )
--- services/client/src/components/__tests__/Exercises.test.jsx
+++ services/client/src/components/__tests__/Exercises.test.jsx
@@ -31,8 +31,6 @@ test('Exercises renders properly when not authenticated', () => {
 
   const wrapper = shallow(<Exercises isAuthenticated={false}/>);
   wrapper.setState({exercises : exercises});
-  const heading = wrapper.find('h5');
-  expect(heading.length).toBe(1);
 
   const alert = wrapper.find('.notification');
   expect(alert.length).toBe(1);
@@ -48,8 +46,6 @@ test('Exercises renders properly when authenticated', () => {
 
   const wrapper = shallow(<Exercises isAuthenticated={true}/>);
   wrapper.setState({exercises : exercises});
-  const heading = wrapper.find('h5');
-  expect(heading.length).toBe(1);
 
   const alert = wrapper.find('.notification');
   expect(alert.length).toBe(0);
