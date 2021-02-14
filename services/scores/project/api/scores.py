5b57580970800afc4b84344d03197ef23e0d42d0
Nicolás Castro Leal <npcastrol@gmail.com>
2021-02-14 15:23:45 -0300

    Add get all route to Scores API

--- services/scores/project/api/scores.py
+++ services/scores/project/api/scores.py
@@ -1,7 +1,29 @@
 from flask import Blueprint, jsonify
+from flask_restful import Resource, Api
+
+from project.api.models import Score
 
 
 scores_blueprint = Blueprint('scores', __name__)
+api = Api(scores_blueprint)
+
+
+class ScoresList(Resource):
+
+    def get(self):
+        """Get all scores"""
+
+        response_object = {
+            'status': 'success',
+            'data': {
+                'scores': [score.to_json() for score in Score.query.all()]
+            }
+        }
+
+        return response_object, 200
+
+
+api.add_resource(ScoresList, '/scores')
 
 
 @scores_blueprint.route('/scores/ping', methods=['GET'])
--- services/scores/project/tests/test_scores_api.py
+++ services/scores/project/tests/test_scores_api.py
@@ -1,9 +1,32 @@
 import json
 
 from project.tests.base import BaseTestCase
+from project.tests.utils import add_score
 
 
 class TestScoresService(BaseTestCase):
+
+    def test_get_all_scores(self):
+        """Ensure get all scores behaves correctly."""
+        add_score(1, 1, True)
+        add_score(2, 1, False)
+
+        with self.client:
+            response = self.client.get('/scores')
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('success', data['status'])
+
+            self.assertEqual(len(data['data']['scores']), 2)
+
+            self.assertEqual(1, data['data']['scores'][0]['user_id'])
+            self.assertEqual(1, data['data']['scores'][0]['exercise_id'])
+            self.assertEqual(True, data['data']['scores'][0]['correct'])
+
+            self.assertEqual(2, data['data']['scores'][1]['user_id'])
+            self.assertEqual(1, data['data']['scores'][1]['exercise_id'])
+            self.assertEqual(False, data['data']['scores'][1]['correct'])
 
     def test_ping(self):
         """Ensure the /ping route behaves correctly."""
